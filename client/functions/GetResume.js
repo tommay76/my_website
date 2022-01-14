ad0c92cd077bf6c0f7738ee51a9752d4874d3cea
tommay76 <z5164445@ad.unsw.edu.au>
2022-01-14 22:44:12 +1100

    fixed gitignore typo, removed build and node_module directories

--- client/functions/GetResume.js
+++ client/functions/GetResume.js
@@ -1,18 +1,18 @@
 // Netlify Function
 // Serves a PDF File 
 
-import express from 'express'
-import bodyParser from 'body-parser'
-import serverless from 'serverless-http'
-import fs from 'fs'
-// import { Mongoose } from 'mongoose';
-import cors from 'cors';
-// const PORT = process.env.PORT || 3001
+const express = require ('express')
+const bodyParser = require ('body-parser')
+const serverless = require ('serverless-http')
+const fs = require ('fs')
+// const { Mongoose } = require 'mongoose';
+const cors = require ('cors');
+const PORT = process.env.PORT || 3001
 
 const app = express()
 app.use(cors())
 app.get('/api/resume', cors(), (req, res) => {
-  var stream = fs.createReadStream('./PDFs/ThomasDoyleResume.pdf')
+  var stream = fs.createReadStream('../src/PDFs/ThomasDoyleResume.pdf')
   var filename = 'ThomasDoyleResume.pdf'
   filename = encodeURIComponent(filename)
   res.setHeader('Content-disposition', 'inline; filename="' + filename + '"')
@@ -21,5 +21,5 @@ app.get('/api/resume', cors(), (req, res) => {
   console.log('Resume fetched from API at ' + new Date())
 })
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
-
+// app.listen(PORT, console.log("server listeneing"))
 exports.handler = serverless(app)
